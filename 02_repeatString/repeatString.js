2d4549c203765daa21a200ff8957d33af5d1e36d
mohamed <95254152+mamohamed055@users.noreply.github.com>
2023-04-08 04:46:12 -0400

    Solve exercises 1&2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    addString = string;
+    if (num == 0){
+        return '';
+    }
+    else if (num<0){
+        return 'ERROR';
+    }
+    for (let i=0; i<num-1; i++)
+    {
+        string += addString;
+    }
+    return string;
 };
 
 // Do not edit below this line
