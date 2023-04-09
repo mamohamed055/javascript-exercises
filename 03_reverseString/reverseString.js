28ad84e074e39ed20855db6c0810fa8f1591f198
mohamed <95254152+mamohamed055@users.noreply.github.com>
2023-04-09 19:57:21 -0400

    Solve exercise 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+let len = string.length;
+let str = '';
+for (let i=1; i<=len; i++){
+    str += string.charAt(len-i);
+    console.log(str);
+}
+return str;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
 const removeFromArray = function() {
+    let arr = arguments[0];
+    let lenArr = arguments[0].length;
+    let lenArgs = arguments.length;
 
+    for (i=1; i<lenArgs; i++){
+        for (let j=0; j<lenArr; j++){
+            if (arr[j] === arguments[i]){
+                arr.splice(j,1);
+            }
+            else {
+                continue;
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
