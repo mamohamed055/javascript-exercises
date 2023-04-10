6f5c3b0e160a1b96a4cc2f1cad3a5b71d765840b
mohamed <95254152+mamohamed055@users.noreply.github.com>
2023-04-09 21:00:41 -0400

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,35 @@
 const sumAll = function() {
+let a = arguments[0];
+let b = arguments[1];
+let min ;
+let max ;
 
+if (a<0 || b<0){
+    return 'ERROR';
+}
+else if (typeof(a)!= 'number'|| typeof(b)!= 'number'){
+    return 'ERROR';
+}
+
+if (a>=b){
+    max = a;
+    min = b;
+}
+else if (b>a){
+    max = b;
+    min = a;
+}
+
+let sum = min;
+let add = min+1;
+let delta = max - min;
+let i = 0;
+while (i<delta){
+    sum+=add;
+    i++;
+    add++;
+}
+return sum;
 };
 
 // Do not edit below this line
