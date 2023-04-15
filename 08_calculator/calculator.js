b81361510a0e96daa4c3dcf1cd8b34fdf0eadb0f
mohamed <95254152+mamohamed055@users.noreply.github.com>
2023-04-15 01:06:54 -0400

    Solve exercise 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+	for (let i = 0; i < arr.length ; i++){
+    total += arr[i];
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((accumulator, currentValue) => accumulator * currentValue, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let i = a;
+  let result = 1;
+  if (a===0) return 1;
+  while (i>0){
+    result *= i;
+    i--;
+  }
+  return result;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,26 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = [];
+    let revArr = [];
+    const capLetters = [...Array(26)].map((val, i) => String.fromCharCode(i + 65));
+    str = str.toUpperCase();
+    for (let i=0; i<str.length; i++){
+        //if str.charAt(i) is a letter add to arr
+        if (capLetters.includes(str.charAt(i))) {
+            arr.push(str.charAt(i));
+        }
+        else continue;
+    }
+    //reverse arr and push to revArr
+    let j = arr.length - 1;
+    for (let i=0; i<arr.length; i++){
+        revArr.push(arr[j]);
+        j--;
+    }
+    //compare the two arrays and see if they are the same (return true)
+    for (let i=0; i<arr.length; i++){
+        if (arr[i] !== revArr[i]) return false; 
+    }
+    return true;
 };
 
 // Do not edit below this line
